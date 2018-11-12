ed0734c279175ef3fb2673076a2c41ce43c90d16
kchapl <kchapl@users.noreply.github.com>
2018-11-12 21:15:46 +0000

    Lint test

--- test/conf/RoutingTest.scala
+++ test/conf/RoutingTest.scala
@@ -1,15 +1,19 @@
 package conf
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers, OptionValues}
 import org.scalatestplus.play.guice._
 import play.api.test.Helpers._
 import play.api.test._
 
-class RoutingTest extends FlatSpec with GuiceOneAppPerTest with Matchers {
+class RoutingTest
+    extends FlatSpec
+    with GuiceOneAppPerTest
+    with Matchers
+    with OptionValues {
 
   "/" should "render index" in {
     val request = FakeRequest(GET, "/")
-    val index   = route(app, request).get
+    val index   = route(app, request).value
     status(index) shouldBe OK
   }
 }
